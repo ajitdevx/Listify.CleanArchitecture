b3971c767da9cd78bbddb88b10cff41b724a808b
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 19:15:10 +0530

    TodoList Command & Handler

---
+++ src/Listify.Application/TodoList/Commands/CreateTodoList/CreateTodoListCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Application.TodoList.Commands.CreateTodoList
+{
+
+    public class CreateTodoListCommand : IRequest<int>
+    {
+        public string? Title { get; set; }
+
+        public CreateTodoListCommand(string title) => Title = title;
+    }
+}
---
+++ src/Listify.Application/TodoList/Commands/CreateTodoList/CreateTodoListCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Application.TodoList.Commands.CreateTodoList
+{
+    internal class CreateTodoListCommandHandler : IRequestHandler<CreateTodoListCommand, int>
+    {
+        public Task<int> Handle(CreateTodoListCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
