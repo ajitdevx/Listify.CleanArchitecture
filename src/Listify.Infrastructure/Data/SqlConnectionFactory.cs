3621347d6b6914f3c5c709c0b60f92029aeca13b
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 19:34:18 +0530

    Establishing database connection

---
+++ src/Listify.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using Listify.Domain.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Infrastructure.Data
+{
+    internal class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly string? _connectionString;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SqlServer");
+
+            if (string.IsNullOrEmpty(_connectionString))
+                throw new ArgumentNullException(_connectionString, "Connection string can't be null");
+
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ src/Listify.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Listify.Domain.Entities;
+using Listify.Domain.Interfaces;
+
+namespace Listify.Infrastructure.Data
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+        private readonly IRepository<TodoListEntity> _todoLists;
+        private readonly IRepository<TodoItemEntity> _todoItems;
+        public UnitOfWork(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+            _todoLists = _todoLists ??= new Repository<TodoListEntity>(_sqlConnectionFactory);
+            _todoItems = _todoItems ??= new Repository<TodoItemEntity>(_sqlConnectionFactory);
+        }
+        public IRepository<TodoListEntity> TodoLists => _todoLists;
+        public IRepository<TodoItemEntity> TodoItems => _todoItems;
+
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
