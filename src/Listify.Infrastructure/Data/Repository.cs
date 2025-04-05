5c9c3f76620d402641b0914d6a4a91efb0219e59
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 19:44:32 +0530

    Generic Reusable class

--- src/Listify.Infrastructure/Data/Repository.cs
+++ src/Listify.Infrastructure/Data/Repository.cs
@@ -1,4 +1,5 @@
-﻿using Listify.Domain.Common;
+﻿using Dapper;
+using Listify.Domain.Common;
 using Listify.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -15,29 +16,39 @@ namespace Listify.Infrastructure.Data
         {
             _sqlConnectionFactory = sqlConnectionFactory;
         }
-        public Task AddAsync(T entity)
+        public async Task<T?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var query = $"SELECT * FROM {typeof(T).Name}s WHERE Id = @Id";
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            return await connection.QuerySingleOrDefaultAsync<T>(query, new { Id = id });
         }
 
-        public Task DeleteAsync(int id)
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var query = $"SELECT * FROM {typeof(T).Name}s";
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            return await connection.QueryAsync<T>(query);
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task AddAsync(T entity)
         {
-            throw new NotImplementedException();
+            var query = $"INSERT INTO {typeof(T).Name}s VALUES (@Entity)";
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            await connection.ExecuteAsync(query, new { Entity = entity });
         }
 
-        public Task<T?> GetByIdAsync(int id)
+        public async Task UpdateAsync(T entity)
         {
-            throw new NotImplementedException();
+            var query = $"UPDATE {typeof(T).Name}s SET @Entity WHERE Id = @Id";
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            await connection.ExecuteAsync(query, new { Entity = entity });
         }
 
-        public Task UpdateAsync(T entity)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var query = $"DELETE FROM {typeof(T).Name}s WHERE Id = @Id";
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            await connection.ExecuteAsync(query, new { Id = id });
         }
     }
 }
