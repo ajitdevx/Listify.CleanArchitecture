c30bf3d810964e591a2f48a5c7ac7d4c31a01378
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 17:43:46 +0530

    Configure Swagger

--- src/Listify.Web/ConfigureServices.cs
+++ src/Listify.Web/ConfigureServices.cs
@@ -1,4 +1,7 @@
-﻿namespace Listify.Web
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Listify.Web
 {
     public static class ConfigureServices
     {
@@ -7,7 +10,21 @@
             // Add services to the container.
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = $"TodoMinimalApi - V1",
+                        Description = "An example implementation of Minimal API in .NET 8.",
+                    });
+
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+                options.DocInclusionPredicate((name, api) => true);
+            });
 
             return services;
         }
