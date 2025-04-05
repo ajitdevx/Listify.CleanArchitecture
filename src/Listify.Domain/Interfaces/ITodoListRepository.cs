4f8df7a23163b304bcdbe5003e23407c9ea978f0
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 23:05:01 +0530

    ITodo List Repository

---
+++ src/Listify.Domain/Interfaces/ITodoListRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Domain.Interfaces
+{
+    public interface ITodoListRepository
+    {
+        Task<int> AddAsync(TodoListEntity entity);        
+        Task DeleteAsync(int id);
+        Task<IEnumerable<TodoListEntity>> GetAllAsync();
+        Task<TodoListEntity?> GetByIdAsync(int id);
+        Task UpdateAsync(TodoListEntity entity);
+    }
+}
