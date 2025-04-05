dd8fe9c61d95c1c0ec3cf6cec20f7d5541e639bb
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 18:24:25 +0530

    Global usings

--- src/Listify.Web/GlobalUsings.cs
+++ src/Listify.Web/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Listify.Web;
 global using Listify.Application;
 global using Listify.Infrastructure;
 global using Listify.Web.Infrastructure;
+global using MediatR;
+global using Ardalis.GuardClauses;
