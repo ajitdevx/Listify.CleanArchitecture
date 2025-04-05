77ef18ee3382631fbbbab549b0460da8dddabeca
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-05 18:24:14 +0530

    WeatherForecast endpoint change

--- src/Listify.Web/Endpoints/WeatherForecasts.cs
+++ src/Listify.Web/Endpoints/WeatherForecasts.cs
@@ -1,33 +1,18 @@
-﻿using Listify.Web.Infrastructure;
-using Microsoft.AspNetCore.Builder;
+﻿using Listify.Application.WeatherForecast.Queries.GetWeatherForecasts;
 
 namespace Listify.Web.Endpoints
 {
     public class WeatherForecasts : EndpointGroupBase
     {
-        private readonly string[] summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         public override void Map(WebApplication app)
         {
-            app.MapGet("/api/weatherforecast", GetWeatherForecasts)
-                .WithName(nameof(GetWeatherForecasts))
-                .WithOpenApi();
+            app.MapGroup(this)
+                .MapGet(GetWeatherForecasts);
         }
 
-        public IEnumerable<WeatherForecast> GetWeatherForecasts()
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts(ISender sender)
         {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-            .ToArray();
-            return forecast;
+            return await sender.Send(new GetWeatherForecastQuery());
         }
     }
 }
