62978fe9c564c8eb5d3082e3b7dc838cea00b07f
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-06 11:37:55 +0530

    Delete Todo Lists

---
+++ src/Listify.Application/TodoList/Commands/DeleteTodoList/DeleteTodoListCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Application.TodoList.Commands.DeleteTodoList
+{
+    public class DeleteTodoListCommand : IRequest
+    {
+        public int Id { get; private set; }
+        public DeleteTodoListCommand(int id) => Id = id;
+    }
+}
---
+++ src/Listify.Application/TodoList/Commands/DeleteTodoList/DeleteTodoListCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Ardalis.GuardClauses;
+using Listify.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Application.TodoList.Commands.DeleteTodoList
+{
+    public class DeleteTodoListCommandHandler : IRequestHandler<DeleteTodoListCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeleteTodoListCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(DeleteTodoListCommand request, CancellationToken cancellationToken)
+        {
+            var todoList = await _unitOfWork.TodoLists.GetByIdAsync(request.Id);
+            Guard.Against.NotFound(request.Id, todoList);
+
+            await _unitOfWork.TodoLists.DeleteAsync(request.Id);
+        }
+    }
+}
