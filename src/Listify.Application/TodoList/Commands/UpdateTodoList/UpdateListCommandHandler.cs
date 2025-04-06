153b00af5c2966766b53b5639dad2d5543b1d489
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-04-06 11:38:08 +0530

    Update TodoList

---
+++ src/Listify.Application/TodoList/Commands/UpdateTodoList/UpdateListCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+using Listify.Application.TodoList.Commands.CreateTodoList;
+using Listify.Domain.Entities;
+using Listify.Domain.Interfaces;
+using MediatR;
+
+namespace Listify.Application.TodoList.Commands.UpdateTodoList
+{
+    internal class UpdateListCommandHandler : IRequestHandler<UpdateTodoListCommandWithId>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UpdateListCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(UpdateTodoListCommandWithId request, CancellationToken cancellationToken)
+        {
+            var todoList = await _unitOfWork.TodoLists.GetByIdAsync(request.Id);
+            
+            Guard.Against.NotFound(request.Id, todoList);
+
+            var entity = new TodoListEntity
+            {
+                Title = request.Title,
+                Description = request.Description,
+            };
+
+            await _unitOfWork.TodoLists.UpdateAsync(entity, request.Id);
+        }
+    }
+}
---
+++ src/Listify.Application/TodoList/Commands/UpdateTodoList/UpdateTodoListCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listify.Application.TodoList.Commands.UpdateTodoList
+{
+    public class UpdateTodoListCommand : IRequest
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public UpdateTodoListCommand(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+
+    public class UpdateTodoListCommandWithId : UpdateTodoListCommand
+    {
+        public int Id { get; set; }
+        public UpdateTodoListCommandWithId(string title, string description, int id) : base(title, description)
+        {
+            Id = id;
+        }
+    }
+}
